a471a130f4118787a65b5bd6b266576d7cd2b409
Tim Craig <TimCraig@Druai.com>
2020-01-18 01:07:36 -0800

    Added new functions.

--- rosdev.bash
+++ rosdev.bash
@@ -1,52 +1,77 @@
 #!/bin/bash
 
-#Change the working directory to the ROS distro include directory
+# Convenince funcions for working with ROS2.
+# Source into your terminal session to use or for regular use source in your .bashrc
+# Author Tim Craig  (TimCraig@Druai.com)  2020  
+# No warranties implied or otherwise.  
+
+# Change the working directory to the ROS distro include directory
 rosinc() {
    pushd /opt/ros/${ROS_DISTRO}/include
 }
 
-#Change the working directory to the ROS distro rclcpp include directory
+# Change the working directory to the ROS distro rclcpp include directory
 rclcpp() {
    pushd /opt/ros/${ROS_DISTRO}/include/rclcpp
 }
 
-#Change directories to the specified ROS2 workspace and source ROS2 and local setup files
-ros2ws() {
-   #Change to the specified directory
-   cd ${HOME}/$1
-   #Source the ROS2 environment
-   #Don't think this is necessary once WS is set up - TTC 
- #  source /opt/ros/$2/setup.bash
-   #Source the workspace environment
-   source ./install/setup.bash
-}
-
-#Change the working directory to our colcon_ws ROS2 workspace
-colconws() {
-   ros2ws ros2/colcon_ws
-}
-
-#Change the working directory to our test_ws ROS2 workspace
-testws() {
-   ros2ws ros2/test_ws
-}
-
-#Change the working directory to our test_ws ROS2 workspace
-druaiws() {
-   ros2ws ros2/druai_ws
-}
-
-#Source ROS2 Setup
+# Source ROS2 Setup for the specified distro
+# Convenient for switching between multiple distros
 ROS2Setup() {
    source /opt/ros/$1/setup.bash
 }
 
-#Setup ROS2 Dashing
+# Setup ROS2 Dashing
 Dashing() {
    ROS2Setup dashing
 }
 
-#Setup ROS2 Eloquent
+# Setup ROS2 Eloquent
 Eloquent() {
    ROS2Setup eloquent
 }
+
+# Print the current ROS environment variables
+ROSEnv() {
+   env | grep "ROS"
+}
+
+# Print where the current ROS distro is mentioned in the environment
+ROSDistroEnv() {
+   env | grep ${ROS_DISTRO}
+}
+
+# Change directories to the specified ROS2 workspace and source local setup files
+ros2ws() {
+   CurrentROS2WS=$1
+   #Change to the specified directory
+   cd ${HOME}/$1
+   #Source the workspace environment
+   source /usr/share/gazebo/setup.sh
+   source ./install/setup.bash
+}
+
+#*************
+# Add your particular workspaces here
+#*************
+
+# Change the working directory to our colcon_ws ROS2 workspace
+colconws() {
+   ros2ws ros2/colcon_ws
+}
+
+# Change the working directory to our test_ws ROS2 workspace
+testws() {
+   ros2ws ros2/test_ws
+}
+
+# Change the working directory to our druai_ws ROS2 workspace
+druaiws() {
+   ros2ws ros2/druai_ws
+}
+
+# Print where the current ROS workspace is mentioned in the environment
+ROSWSEnv() {
+   env | grep ${CurrentROS2WS}
+}
+
